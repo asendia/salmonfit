3a0062f875f0d3a41700d16e293fbf378e29b4c1
Asendia Mayco <asendia@traveloka.com>
2022-02-23 02:46:43 +0700

    Move CSP to meta tag

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,26 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    amp: true,
     adapter: adapter({
       pages: 'build',
       fallback: null
     }),
+    amp: true,
+    // "default-src * data: blob:; script-src blob: 'self' https://cdn.ampproject.org/v0.js https://cdn.ampproject.org/v0/ https://cdn.ampproject.org/viewer/ https://cdn.ampproject.org/rtv/; object-src 'none'; style-src 'unsafe-inline' https://cdn.ampproject.org/rtv/ https://cdn.materialdesignicons.com https://cloud.typography.com https://fast.fonts.net https://fonts.googleapis.com https://maxcdn.bootstrapcdn.com https://p.typekit.net https://use.fontawesome.com https://use.typekit.net; report-uri https://csp-collector.appspot.com/csp/amp"
+    csp: {
+      mode: "hash",
+      directives: {
+        'default-src': ['*', 'data:', 'blob:'],
+        'script-src': ['blob:', 'self', 'https://cdn.ampproject.org/v0.js', 'https://cdn.ampproject.org/v0/', 
+          'https://cdn.ampproject.org/viewer/', 'https://cdn.ampproject.org/rtv/'],
+        'object-src': ['none'],
+        'style-src': ['unsafe-inline', 'https://cdn.ampproject.org/rtv/', 'https://cdn.materialdesignicons.com',
+          'https://cloud.typography.com', 'https://fast.fonts.net', 'https://fonts.googleapis.com',
+          'https://maxcdn.bootstrapcdn.com', 'https://p.typekit.net', 'https://use.fontawesome.com',
+          'https://use.typekit.net'],
+        'report-uri': ['https://csp-collector.appspot.com/csp/amp']
+      }
+    },
     prerender: {
       // With the default value, somehow sveltekit crawls non page components like
       // service-worker.js, favicon.png and it results in error: "404 /service-worker.js (linked from /)"
