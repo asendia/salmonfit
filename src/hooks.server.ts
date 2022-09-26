3c813fc82b7a2b1a177f151390a643da15107228
Asendia Mayco <asendia@icloud.com>
2022-09-26 21:09:29 +0700

    Create CSP object for future csp config in sveltekit

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,13 @@
-import type { Handle } from '@sveltejs/kit';
+import { cspDirectives, generateCSPHeaderValue } from '$lib/csp/csp';
 import * as amp from '@sveltejs/amp';
+import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
 	if (event.url.pathname === '/') {
 		const response = await resolve(event, {
 			transformPageChunk: ({ html, done }) => {
 				if (done) {
 					const ampHtml = amp.transform(html);
-					// Add amp-carousel
+					// Add amp-carousel, current version of sveltekit removes amp-carousel for some reason
 					const ampHtmlWithCarousel = ampHtml.replace(
 						'<script async src="https://cdn.ampproject.org/v0.js"></script>',
 						'<script async src="https://cdn.ampproject.org/v0.js"></script><link rel="preload" as="script" href="https://cdn.ampproject.org/v0/amp-carousel-0.2.js"><script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.2.js"></script>'
@@ -20,10 +21,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 		response.headers.set('x-content-type-options', 'nosniff');
 		response.headers.set('referrer-policy', 'strict-origin-when-cross-origin');
 		response.headers.set('permissions-policy', 'fullscreen=()');
-		response.headers.set(
-			'content-security-policy',
-			"default-src * data: blob:; script-src blob: 'self' https://cdn.ampproject.org/v0.js https://cdn.ampproject.org/v0/ https://cdn.ampproject.org/viewer/ https://cdn.ampproject.org/rtv/; object-src 'none'; style-src 'unsafe-inline' https://cdn.ampproject.org/rtv/ https://cdn.materialdesignicons.com https://cloud.typography.com https://fast.fonts.net https://fonts.googleapis.com https://maxcdn.bootstrapcdn.com https://p.typekit.net https://use.fontawesome.com https://use.typekit.net; report-uri https://csp-collector.appspot.com/csp/amp"
-		);
+		// Temporary until sveltekit support CSP with AMP
+		response.headers.set('content-security-policy', generateCSPHeaderValue(cspDirectives));
 		response.headers.set('cache-control', 's-maxage=60, stale-while-revalidate');
 		return response;
 	}
---
+++ src/lib/csp/csp.ts
@@ -0,0 +1,37 @@
+import type { CspDirectives } from '@sveltejs/kit/types/private';
+
+// Temporary until sveltekit support CSP with AMP
+
+// Default values from https://amp.dev/documentation/guides-and-tutorials/optimize-and-measure/secure-pages/
+export const cspDirectives: CspDirectives = {
+	'default-src': ['http://localhost:*', 'https://salmonfit.com', 'data:', 'blob:'],
+	'script-src': [
+		'blob:',
+		'self',
+		'https://cdn.ampproject.org/v0.js',
+		'https://cdn.ampproject.org/v0/',
+		'https://cdn.ampproject.org/rtv/'
+	],
+	'object-src': ['none'],
+	'font-src': ['https://fonts.gstatic.com/s/'],
+	'style-src': ['unsafe-inline']
+};
+
+export function generateCSPHeaderValue(directives: CspDirectives) {
+	const headerVal: Array<string> = [];
+	Object.keys(cspDirectives).forEach((d) => {
+		const directive = directives[d as keyof CspDirectives];
+		if (!Array.isArray(directive)) return;
+		const v = directive
+			.map((i) => {
+				// CSP BaseSource
+				if (['self', 'unsafe-eval', 'unsafe-hashes', 'unsafe-inline', 'none'].includes(i)) {
+					return `'${i}'`;
+				}
+				return i;
+			})
+			.join(' ');
+		headerVal.push(`${d} ${v};`);
+	});
+	return headerVal.join(' ');
+}
